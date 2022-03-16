3d67d1730f92a9eea0b257de429083e41f724027
RenanSM <renan.psm2018@gmail.com>
2022-03-16 11:20:16 -0300

    Finalização da atividade

--- src/main/java/ren/hnt/exemplotrycatch/TratamentoArrayOutOfBounds.java
+++ src/main/java/ren/hnt/exemplotrycatch/TratamentoArrayOutOfBounds.java
@@ -11,5 +11,21 @@ package ren.hnt.exemplotrycatch;
 public class TratamentoArrayOutOfBounds {
     public static void main(String[] args) {
         
+        int[] matrizA = new int[25];
+        
+        try{
+            for(int i=1;i<150;i++){
+            matrizA[i] = i;
+            System.out.println("Posição "+(i-1)+"º, valor = "+matrizA[i]);
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.printf("%nExceção encontrada: %s%n", e);
+            System.out.println("Excesso no limite de elementos na matriz");
+            
+        }
+        
+        
+        
     }
 }
